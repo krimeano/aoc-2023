25c99c0d420d0bfc10874871950dd3c5fc505c1f
Sergii Paliutin <spalutin@flutterint.com>
2023-12-02 13:43:50 +0000

    day 2 tests

--- days/day02/test_game.py
+++ days/day02/test_game.py
@@ -1,17 +1,18 @@
 from .game import Game
 
-"""
-Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
-"""
+# in game 1 is 48. In games 2-5 it was 12, 1560, 630, and 36, respectively
+
+test_cases = [
+    ["Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", True, 48],
+    ["Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", True, 12],
+    ["Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", False, 1560],
+    ["Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", False, 630],
+    ["Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", True, 36]
+]
 
 
 def test_games():
-    assert Game('Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green').is_possible
-    assert Game('Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue').is_possible
-    assert not Game('Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red').is_possible
-    assert not Game('Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red').is_possible
-    assert Game('Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green').is_possible
+    for [line, is_possible, power] in test_cases:
+        game = Game(line)
+        assert game.is_possible == is_possible
+        assert game.power == power
