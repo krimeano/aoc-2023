a7d454896b50aaf4e5ef25a5b62f915ff52ffda9
Sergii Paliutin <spalutin@flutterint.com>
2024-11-23 22:30:52 +0000

    24.1

--- days/day24/solution.py
+++ days/day24/solution.py
@@ -9,41 +9,11 @@ def sign(number: float):
     return 0
 
 
-class HailStone:
-    def __init__(self, init_str: str, radius: int, origin=0):
-        [self.xyz, self.vvv] = [tuple(int(y) for y in x.split(', ')) for x in init_str.split(' @ ')]
-        self.radius = radius
-        self.xyz = tuple(x - origin for x in self.xyz)
-        self.enters = self.calculate_enters()
-        self.leaves = self.calculate_leaves()
-
-    def __str__(self):
-        a = '[{0}, {1}]@({2}, {3})'.format(self.xyz[0], self.xyz[1], self.vvv[0], self.vvv[1])
-        b = '[{0}, {1}]>[{2}, {3}]'.format(self.enters[0], self.enters[1], self.leaves[0], self.leaves[1])
-        return a + ': ' + b
-
-    def calculate_enters(self) -> tuple[float, float]:
-        t = max(0, *[self.calculate_crossing_time(ix, -1) for ix in range(2)])
-        return self.calculate_coords(t)
-
-    def calculate_leaves(self) -> tuple[float, float]:
-        t = min(*[self.calculate_crossing_time(ix) for ix in range(2)])
-        return self.calculate_coords(t)
-
-    def calculate_crossing_time(self, ix: int, factor=1) -> float:
-        return (factor * sign(self.vvv[ix]) * self.radius - self.xyz[ix]) / self.vvv[ix]
-
-    def calculate_coords(self, t: float) -> tuple[float, float]:
-        return self.calculate_coord(0, t), self.calculate_coord(1, t)
-
-    def calculate_coord(self, ix: int, t: float) -> float:
-        return self.xyz[ix] + t * self.vvv[ix]
-
-
 def ab_cross_cd(ab: tuple[float, float], cd: tuple[float, float]) -> float:
     result = ab[0] * cd[1] - ab[1] * cd[0]
     if not result:
-        raise 'COLLINEAR!'
+        print(ab, cd)
+        raise BaseException('COLLINEAR!')
     return result
 
 
@@ -51,27 +21,94 @@ def abc_cross(a: tuple[float, float], b: tuple[float, float], c: tuple[float, fl
     return ab_cross_cd((b[0] - a[0], b[1] - a[1]), (c[0] - a[0], c[1] - a[1]))
 
 
-def get_orientations(ab: HailStone, cd: HailStone) -> tuple[float, float]:
-    return abc_cross(ab.enters, ab.leaves, cd.enters), abc_cross(ab.enters, ab.leaves, cd.leaves)
+class HailStone:
+    def __init__(self, init_str: str, radius: int, origin=0, verbose=False):
+        self.verbose = verbose
+        self.radius = radius
+        [self.start, self.speed] = [tuple(int(y) for y in x.split(', ')) for x in init_str.split(' @ ')]
+        self.start = tuple(x - origin for x in self.start)
+        self.fixed = ab_cross_cd((self.start[0], self.start[1]), (self.speed[0], self.speed[1]))
 
+    def __str__(self):
+        a = '[{0}, {1}]@({2}, {3})'.format(self.start[0], self.start[1], self.speed[0], self.speed[1])
+        return a
 
-def test_crossing(a: HailStone, b: HailStone, verbose: bool) -> bool:
-    if verbose:
-        print('A ', a)
-        print('B ', b)
-    ab = get_orientations(a, b)
-    ba = get_orientations(b, a)
-    result = sign(ab[0]) != sign(ab[1]) and sign(ba[0]) != sign(ba[1])
-    if verbose:
-        print(ab, ba, result)
-    return result
+    def calculate_crossing_time(self, ix: int, factor=1) -> float:
+        return (factor * sign(self.speed[ix]) * self.radius - self.start[ix]) / self.speed[ix]
+
+    def calculate_coords(self, t: float) -> tuple[float, float]:
+        return self.calculate_coord(0, t), self.calculate_coord(1, t)
+
+    def calculate_coord(self, ix: int, t: float) -> float:
+        return self.start[ix] + t * self.speed[ix]
+
+    def test_crossing(self, other) -> bool:
+        if self.verbose:
+            print('A ', self)
+            print('B ', other)
+
+        if self.parallel_xy_projection(other):
+            if self.verbose:
+                print('PARALLEL. never cross')
+            return False
+
+        result = True
+        crossing = self.find_crossing(other)
+        if self.verbose:
+            print('Cross at', crossing, end='')
+        if not self.test_coords(crossing):
+            if self.verbose:
+                print(' outside the box', end='')
+            result = False
+        elif self.verbose:
+            print(' inside the box', end='')
+
+        in_the_past = False
+        if not self.test_time(crossing):
+            in_the_past = True
+            if self.verbose:
+                print(' in the past for A', end='')
+
+        if not other.test_time(crossing):
+            if self.verbose:
+                print(' in the past for B', end='')
+            in_the_past = True
+        if in_the_past:
+            if self.verbose:
+                print()
+            result = False
+        elif self.verbose:
+            print(' in the future')
+
+        if self.verbose:
+            print(result)
+
+        return result
+
+    def parallel_xy_projection(self, other) -> bool:
+        return self.speed[0] * other.speed[1] == self.speed[1] * other.speed[0]
+
+    def find_crossing(self, other) -> tuple[float, float]:
+        s = ab_cross_cd((self.speed[0], self.speed[1]), (other.speed[0], other.speed[1]))
+        x = (other.fixed * self.speed[0] - self.fixed * other.speed[0]) / s
+        y = (other.fixed * self.speed[1] - self.fixed * other.speed[1]) / s
+        return x, y
+
+    def test_coords(self, crossing: tuple[float, float]) -> bool:
+        return -self.radius <= crossing[0] <= self.radius and -self.radius <= crossing[1] <= self.radius
+
+    def test_time(self, crossing: tuple[float, float]) -> bool:
+        return self.validate_time_ort(crossing, 0) and self.validate_time_ort(crossing, 1)
+
+    def validate_time_ort(self, crossing: tuple[float, float], ix: int) -> bool:
+        return crossing[ix] == self.start[ix] or sign(self.speed[ix]) == sign(crossing[ix] - self.start[ix])
 
 
 class SolveDay24x1(SolveDay):
     def solve(self, text_input: str, radius=1 * 10 ** 14, origin=3 * 10 ** 14) -> int:
         hail_stones = []
         for line in self.get_lines(text_input):
-            hail_stone = HailStone(line, radius, origin)
+            hail_stone = HailStone(line, radius, origin, self.verbose)
             if self.verbose:
                 print(hail_stone)
             hail_stones.append(hail_stone)
@@ -81,7 +118,7 @@ class SolveDay24x1(SolveDay):
             for jy in range(ix + 1, len(hail_stones)):
                 if self.verbose:
                     print()
-                total += test_crossing(hail_stones[ix], hail_stones[jy], self.verbose)
+                total += hail_stones[ix].test_crossing(hail_stones[jy])
 
         return total
 
